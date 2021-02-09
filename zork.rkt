471d608a46a1a35d22fde70cd281b9672c5a5b6c
Bran van der Meer <bran.van.der.meer@protonmail.com>
2021-02-09 08:30:07 +0100

    feat: Update strings

--- zork.rkt
+++ zork.rkt
@@ -1,15 +1,32 @@
 #lang racket
 
-(display "Welcome to zork-racket.\n")
-
-(define (handler x)
-  (string-append "I do not understand " x "\n"))
+(display "ZORK-RACKET I\n")
+(display "SPDX identifier: Unlicense\n")
+(display "ZORK is a registered trademark of Infocom, Inc.\n")
 
 ; Read-Eval-Print-Loop
-(let loop ()
-  (display "\n> ")
+(define (loop)
+  (display "\n>")
   (define input (read-line))
   (when (or (equal? input "quit") (equal? input "exit"))
-    (exit))
+    (exit-handler))
   (display (handler input))
   (loop))
+
+(define (handler x)
+  (string-append "I don't know the word \"" x "\"\n"))
+
+(define (exit-handler)
+  (define score "Your score is 0 (total of 350 points), in 0 moves.")
+  (define rank "This gives you the rank of Beginner.")
+  (display (string-append score "\n" rank))
+  (exit))
+
+(define (get-initial-state)
+  (define title "West of House")
+  (define sentence "You are standing in an open field west of a white house, with a boarded front door.\nThere is a small mailbox here.")
+  (string-append "\n" title "\n" sentence "\n"))
+
+; Print initial state, start REPL
+(display (get-initial-state))
+(loop)
